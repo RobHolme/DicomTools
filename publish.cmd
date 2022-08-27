c81df90c9d282e9173791eb8df38dcc32de68d9e
Rob Holme <rob@holme.com.au>
2022-08-27 15:06:23 +0800

    added shortcut for publish commands

---
+++ publish.cmd
@@ -0,0 +1,2 @@
+dotnet publish --configuration release --framework netstandard2.0 --output .\module\lib\netstandard\
+dotnet publish --configuration release --framework netstandard2.0 --output .\module\lib\netframework\
